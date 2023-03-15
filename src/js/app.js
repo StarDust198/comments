dcec49d6c181fe8be6ae6ad600c0887e2edd0c22
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-03-15 15:35:44 +0400

    postListClicks separate

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import { isWebP } from './modules/isWebP.js';
 import { processForm } from './modules/processForm.js';
+import { postListClicks } from './modules/postListClicks.js';
 
 isWebP();
 
@@ -7,25 +8,4 @@ const form = document.forms.form;
 const postList = document.getElementById('post-list');
 
 processForm(form, postList);
-
-postList.addEventListener('click', (e) => {
-  const tgt = e.target.closest('.post-btn');
-  if (!tgt) return;
-
-  switch (tgt.dataset.action) {
-    case 'like':
-      const innerSvg = tgt.querySelector('svg');
-      const fill = innerSvg.getAttribute('fill');
-      innerSvg.setAttribute('fill', fill === 'none' ? '#7653fc' : 'none');
-      break;
-
-    case 'delete':
-      const post = e.target.closest('.post');
-      post.classList.add('animate__fadeOut');
-      post.onanimationend = () => post.remove();
-      break;
-
-    default:
-      throw new Error('Неизвестное действие');
-  }
-});
+postListClicks(postList);
---
+++ src/js/modules/postListClicks.js
@@ -0,0 +1,23 @@
+export const postListClicks = (postList) => {
+  postList.addEventListener('click', (e) => {
+    const tgt = e.target.closest('.post-btn');
+    if (!tgt) return;
+
+    switch (tgt.dataset.action) {
+      case 'like':
+        const innerSvg = tgt.querySelector('svg');
+        const fill = innerSvg.getAttribute('fill');
+        innerSvg.setAttribute('fill', fill === 'none' ? '#7653fc' : 'none');
+        break;
+
+      case 'delete':
+        const post = e.target.closest('.post');
+        post.classList.add('animate__fadeOut');
+        post.onanimationend = () => post.remove();
+        break;
+
+      default:
+        throw new Error('Неизвестное действие');
+    }
+  });
+};
